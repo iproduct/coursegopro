79a9f388821d7e0b538e6df94aabe76125f849fb
Trayan Iliev <office@iproduct.org>
2021-10-03 18:46:27 +0300

    enum example extended

--- 02-go-basics/constants/constants.go
+++ 02-go-basics/constants/constants.go
@@ -12,6 +12,28 @@ const (
 	RoleMask = (1 << (iota)) - 1
 )
 
+type Status int
+
+const (
+	Registered Status = iota
+	Active
+	Disabled
+)
+
+// Returns string representation of the Status
+func (r Status) String() string {
+	switch r {
+	case Registered:
+		return "Registered"
+	case Active:
+		return "Active"
+	case Disabled:
+		return "Disabled"
+	default:
+		return "Invalid status"
+	}
+}
+
 func (r Role) String() string {
 	result := ""
 	if User&r > 0 {
@@ -36,7 +58,12 @@ func main() {
 	fmt.Printf("%s - %[1]d : %[1]T, Mask: %#b\n", User+Manager, RoleMask)
 	fmt.Printf("%s - %[1]d: %[1]T, Mask: %b\n", Manager+Admin, RoleMask)
 	fmt.Printf("%s - %[1]d : %[1]T, Mask: %b\n", Admin, RoleMask)
-	fmt.Printf("%s - %[1]d : %[1]T, Mask: %b\n", Customer, RoleMask)
+	fmt.Printf("%s - %[1]d : %[1]T, Mask: %b\n\n", Customer, RoleMask)
+
+	fmt.Printf("%s - %[1]d : %[1]T\n", Registered)
+	fmt.Printf("%s - %[1]d: %[1]T\n", Active)
+	fmt.Printf("%s - %[1]d : %[1]T\n", Disabled)
+
 }
 
 // fmt package defines interface Stringer as:
