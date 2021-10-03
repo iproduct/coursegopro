9c21c6e0bbe7012ed365bc781e56a85efbabc599
Trayan Iliev <office@iproduct.org>
2021-10-03 14:59:38 +0300

    first lab project added

--- 01-intro/main.go
+++ 01-intro/main.go
@@ -5,9 +5,8 @@ import(
 	"rsc.io/quote"
 	"01-intro/stringutil"
 )
-
+var s = "Hello from Golang!"
 func main() {
-	s := "Hello from Golang!"
 	fmt.Println(s)
 	goquote := quote.Go()
 	fmt.Println(goquote)
