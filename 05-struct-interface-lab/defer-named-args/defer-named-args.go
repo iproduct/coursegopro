13b57f8182e15c91eb147715dfc402c31e7b6419
Trayan Iliev <office@iproduct.org>
2021-10-18 06:37:27 +0300

    examples added/modified

--- 05-struct-interface-lab/defer-named-args/defer-named-args.go
+++ 05-struct-interface-lab/defer-named-args/defer-named-args.go
@@ -18,7 +18,21 @@ func deferDemo() (result1, result2 string) {
 	return
 }
 
+func deferDemoPtr() (result1, result2 *string) {
+	defer func() {
+		fmt.Printf("Inside defered func: %v, %v\n", *result1, *result2) // 2
+		*result1 = "NEW_VALUE"
+	}()
+	fmt.Printf("Inside deferDemoPtr()\n") // 1
+	str1, str2 := "ABCD", "EFGH"
+	return &str1, &str2
+}
+
 func main() {
 	r1, r2 := deferDemo()
 	fmt.Printf("Inside main(): %#v, %#v\n", r1, r2) // 3
+
+	rp1, rp2 := deferDemoPtr()
+	fmt.Printf("Inside main(): %#v, %#v\n", *rp1, *rp2) // 3
+
 }
