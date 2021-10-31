6daefa45a157c807b83068028598956a1b0bee1a
Trayan Iliev <office@iproduct.org>
2021-10-31 17:45:42 +0200

    mysql db setup

---
+++ 09-databases-sql-lab/db-ping-update-execcontext/db-ping-update-execcontext.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@/dbname")
+	if err != nil {
+		panic(err)
+	}
+	// See "Important settings" section.
+	db.SetConnMaxLifetime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+}
