1c2676b9bc25ca3a8958d6a79ba499ca37713c7d
Trayan Iliev <office@iproduct.org>
2021-11-06 19:54:38 +0200

    db updated to golang_projects_gorm

--- 08-databases-gorm/db-gorm-projects-batch/db-gorm-projects-batch.go
+++ 08-databases-gorm/db-gorm-projects-batch/db-gorm-projects-batch.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	dsn := "root:root@tcp(127.0.0.1:3306)/gorm_projects?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:root@tcp(127.0.0.1:3306)/golang_projects_gorm?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
